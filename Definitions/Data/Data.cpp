fbcdf1bfdde9a1248670ac2522dec91ae800fab2
Program <67320703+Program132@users.noreply.github.com>
2024-02-29 21:34:27 +0100

    Data class (contains variables, functions, etc.)

---
+++ Definitions/Data/Data.cpp
@@ -0,0 +1,35 @@
+#include "Data.h"
+
+namespace JDD::Definitions {
+    bool Data::isVariable(const std::string &name) const {
+        if (Variables.contains(name)) {
+            return true;
+        }
+        return false;
+    }
+
+    std::optional<Variable> Data::getVariableFromName(const std::string &name) {
+        if (this->isVariable(name)) {
+            return this->Variables[name];
+        }
+        return std::nullopt;
+    }
+
+    void Data::addVariableToData(const Variable &var) {
+        if (!isVariable(var.name)) {
+            this->Variables[var.name] = var;
+        }
+    }
+
+    void Data::addVariableToData(const std::string& name, const Value& value, const Types& type, const bool& isFinal) {
+        if (!isVariable(name)) {
+            this->Variables[name] = Variable(name, value, type, isFinal);
+        }
+    }
+
+    void Data::removeVariableFromName(const std::string &name) {
+        if (this->isVariable(name)) {
+            this->Variables.erase(name);
+        }
+    }
+}
---
+++ Definitions/Data/Data.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <iostream>
+#include <map>
+#include <optional>
+#include "../Variable/Variable.h"
+
+namespace JDD::Definitions {
+    class Data {
+    public:
+        std::map<std::string, Variable> Variables;
+
+        [[nodiscard]] bool isVariable(const std::string& name) const;
+        std::optional<Variable> getVariableFromName(const std::string& name);
+        void addVariableToData(const Variable& var);
+        void addVariableToData(const std::string& name, const Value& value, const Types& type, const bool& isFinal);
+        void removeVariableFromName(const std::string& name);
+    };
+}
