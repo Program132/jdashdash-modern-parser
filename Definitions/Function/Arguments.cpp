ef8f59eb51c8d558ed4a4d587bb07b93cbf6ffc4
Program <67320703+Program132@users.noreply.github.com>
2024-06-10 20:49:58 +0200

    Function + elements declared in classes

---
+++ Definitions/Function/Arguments.cpp
@@ -0,0 +1,9 @@
+#include "Arguments.h"
+
+JDD::Definitions::Argument::Argument() = default;
+
+JDD::Definitions::Argument::Argument(JDD::Definitions::Types t, JDD::Definitions::Value v, std::string n) {
+    this->type = t;
+    this->value = v;
+    this->name = n;
+}
---
+++ Definitions/Function/Arguments.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <iostream>
+#include "../Types/Types.h"
+#include "../Value/Value.h"
+
+namespace JDD::Definitions {
+    class Argument {
+    public:
+        Argument();
+        Argument(Types t, Value v, std::string n);
+
+        std::string name;
+        Types type;
+        Value value;
+    };
+}
